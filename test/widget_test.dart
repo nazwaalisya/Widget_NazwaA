d56068e36a6e30f6bd76d09dca0f7bf3920b37a0
nazwamp15 <nazwamaulanaputri5@gmail.com>
2024-10-17 07:48:50 +0700

    FIrst Commit

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:rifan_application_1/main.dart';
+import 'package:NazwaMP_Project1/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {

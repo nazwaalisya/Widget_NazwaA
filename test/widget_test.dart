6436929f223d8c7bd457b1698504beec5253fca2
nazwaalisya <nazwanw1965@gmail.com>
2024-10-18 10:11:42 +0700

    FirstCommit

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:Raisya_Ramadhani/main.dart';
+import 'package:Nazwa_A/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
